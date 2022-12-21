01f3def23651504282eb2024c97d9daa698c7489
Łukasz Kędziora <lkedziora@internships.antmicro.com>
2022-12-21 10:45:38 +0100

    test: Fixup test runscript

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,19 +2,22 @@
 set -e -o pipefail
 
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
+BASEDIR="$SCRIPTDIR/../"
+TESTBUILDDIR="$BASEDIR/build/"
+TESTWORKDIR="$BASEDIR/src/test/"
 echo "Script directory: $SCRIPTDIR"
 
 # Create build directory
-mkdir -v -p "$SCRIPTDIR/build/"
-pushd "$SCRIPTDIR/build/"
+mkdir -v -p $TESTBUILDDIR
+pushd $TESTBUILDDIR
 cmake ../ -DTARPARSE_COMPILE_TESTS=
 make
 popd
 
 # Run tests in the proper working directory
-pushd "$SCRIPTDIR/src/test/"
+pushd $TESTWORKDIR
 RC=0
-../../build/tarparse_tests || RC=$?
+$TESTBUILDDIR/tarparse_tests || RC=$?
 popd
 
 exit $RC
